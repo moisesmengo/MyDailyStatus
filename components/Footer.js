67a22034e65fa97cbd229de6ea92c969d1099b16
moisesmengo <moisesmengo@live.com>
2020-04-09 11:47:12 -0300

    estrutura do sistema

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="py-4 text-center bg-gray-400">
+            MyDailyStatus é um projeto criado durante o FUllSTACK LAB do DevPleno <br/>
+            Implementado por: <a href="https://www.linkedin.com/in/mois%C3%A9s-costa-ba3002116/">Moisés Costa</a>
+        </div>   
+    )
+}
+
+export default Footer
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Index = () =>{
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Index
